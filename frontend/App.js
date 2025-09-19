b351d5a80e4e37463ee4aac84424b7e115bf3bd6
rithvik shetty <rithvikshetty2004@gmail.com>
2025-09-19 23:29:07 +0530

    rithvik updated fonts

--- frontend/App.js
+++ frontend/App.js
@@ -4,8 +4,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
-import { Image, View, TouchableOpacity, Text } from 'react-native';
+import { Image, View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { COLORS } from './constants/colors';
+import { useFonts, InstrumentSans_400Regular, InstrumentSans_700Bold } from '@expo-google-fonts/instrument-sans';
 
 // Screens
 import OnboardingScreen from './screens/OnboardingScreen';
@@ -38,9 +39,26 @@ import FishingOptimizationHub from './screens/FishingOptimizationHub';
 import FishingNetsGuide from './screens/FishingNetsGuide';
 import FishingGears from './screens/FishingGears';
 import BaitSelection from './screens/BaitSelection';
+import AnimatedTabBar from './components/AnimatedTabBar';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+
+// Global font setup
+const regularFont = 'InstrumentSans_400Regular';
+const boldFont = 'InstrumentSans_700Bold';
+
+const oldRender = Text.render;
+Text.render = function(...args) {
+  const origin = oldRender.call(this, ...args);
+  const styles = StyleSheet.flatten(origin.props.style);
+  
+  const fontFamily = (styles && (styles.fontWeight === 'bold' || styles.fontWeight >= '700')) ? boldFont : regularFont;
+  
+  return React.cloneElement(origin, {
+    style: [{fontFamily}, origin.props.style]
+  });
+}
 
 const HomeStack = () => (
   <Stack.Navigator>
@@ -72,8 +90,6 @@ const HomeStack = () => (
   </Stack.Navigator>
 );
 
-import AnimatedTabBar from './components/AnimatedTabBar';
-
 const MainTabs = () => (
   <Tab.Navigator
     tabBar={(props) => <AnimatedTabBar {...props} />}
@@ -86,36 +102,47 @@ const MainTabs = () => (
   </Tab.Navigator>
 );
 
-const App = () => (
-  <NavigationContainer>
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Onboarding" component={OnboardingScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="PurposeOnboarding" component={PurposeOnboardingScreen} />
-      <Stack.Screen name="Main" component={MainTabs} />
-      {/* Screens that should be available outside the tab bar but within the main stack */}
-      <Stack.Screen name="TripPlanning" component={TripPlanningScreen} />
-      <Stack.Screen name="News" component={NewsScreen} />
-      <Stack.Screen name="CatchRecord" component={CatchRecordScreen} />
-      <Stack.Screen name="NoFishingZone" component={NoFishingZoneScreen} />
-      <Stack.Screen name="PotentialFishingZone" component={PotentialFishingZoneScreen} />
-      <Stack.Screen name="GpsNavigation" component={GpsNavigationScreen} />
-      <Stack.Screen name="DisasterAlert" component={DisasterAlertScreen} />
-      <Stack.Screen name="ImportantContacts" component={ImportantContactsScreen} />
-      <Stack.Screen name="IBLAlerts" component={IBLAlertsScreen} />
-      <Stack.Screen name="OtherServices" component={OtherServicesScreen} />
-      <Stack.Screen name="SeaSafetyLivelihood" component={SeaSafetyLivelihoodScreen} />
-      <Stack.Screen name="Settings" component={SettingsScreen} />
-      <Stack.Screen name="About" component={AboutScreen} />
-      <Stack.Screen name="EditProfile" component={EditProfileScreen} />
-      <Stack.Screen name="Tracker" component={TrackerScreen} />
-      <Stack.Screen name="ChatScreen" component={ChatScreen} />
-      <Stack.Screen name="FishingOptimizationHub" component={FishingOptimizationHub} />
-      <Stack.Screen name="FishingNetsGuide" component={FishingNetsGuide} />
-      <Stack.Screen name="FishingGears" component={FishingGears} />
-      <Stack.Screen name="BaitSelection" component={BaitSelection} />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+const App = () => {
+  const [fontsLoaded] = useFonts({
+    InstrumentSans_400Regular,
+    InstrumentSans_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null; // Or a loading screen
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="PurposeOnboarding" component={PurposeOnboardingScreen} />
+        <Stack.Screen name="Main" component={MainTabs} />
+        {/* Screens that should be available outside the tab bar but within the main stack */}
+        <Stack.Screen name="TripPlanning" component={TripPlanningScreen} />
+        <Stack.Screen name="News" component={NewsScreen} />
+        <Stack.Screen name="CatchRecord" component={CatchRecordScreen} />
+        <Stack.Screen name="NoFishingZone" component={NoFishingZoneScreen} />
+        <Stack.Screen name="PotentialFishingZone" component={PotentialFishingZoneScreen} />
+        <Stack.Screen name="GpsNavigation" component={GpsNavigationScreen} />
+        <Stack.Screen name="DisasterAlert" component={DisasterAlertScreen} />
+        <Stack.Screen name="ImportantContacts" component={ImportantContactsScreen} />
+        <Stack.Screen name="IBLAlerts" component={IBLAlertsScreen} />
+        <Stack.Screen name="OtherServices" component={OtherServicesScreen} />
+        <Stack.Screen name="SeaSafetyLivelihood" component={SeaSafetyLivelihoodScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+        <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+        <Stack.Screen name="Tracker" component={TrackerScreen} />
+        <Stack.Screen name="ChatScreen" component={ChatScreen} />
+        <Stack.Screen name="FishingOptimizationHub" component={FishingOptimizationHub} />
+        <Stack.Screen name="FishingNetsGuide" component={FishingNetsGuide} />
+        <Stack.Screen name="FishingGears" component={FishingGears} />
+        <Stack.Screen name="BaitSelection" component={BaitSelection} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
